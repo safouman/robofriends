198522628400f1b89a89ec04778dc35e9aea4305
Safwen Daghsen <safouman@gmail.com>
2019-04-16 14:22:35 +0100

    Updates

---
+++ precache-manifest.4279bca2028732e8c5171ea726b5d13b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d7fe85bae3b915098b5",
+    "url": "/robofriends/static/css/main.868c3bf4.chunk.css"
+  },
+  {
+    "revision": "3d7fe85bae3b915098b5",
+    "url": "/robofriends/static/js/main.1b4bd2eb.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "c1759f22a0d28417a56d",
+    "url": "/robofriends/static/css/2.ff33b4e7.chunk.css"
+  },
+  {
+    "revision": "c1759f22a0d28417a56d",
+    "url": "/robofriends/static/js/2.a78f7aff.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "e5f91d36ba6ad8fb4ab0082a5f52d741",
+    "url": "/robofriends/index.html"
+  }
+];
